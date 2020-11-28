c25d627b8be7e6e30fbcc8e97412752d519e8aba
HuynhAnhTai <47748453+HuynhAnhTai@users.noreply.github.com>
2020-11-28 10:45:55 +0700

    refactor sign up

--- source code/main.js
+++ source code/main.js
@@ -28,3 +28,54 @@
 			});
  	})
 }
+
+//Make text input file has name
+function makeTextInputFile(){
+	$(".custom-file-input").on("change", function() {
+	  let fileName = $(this).val().split("\\").pop();
+	  $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+	});
+}
+
+ function signUp(confirmPass, pass, file, mail, username, hoTen, birth, sdt){
+ 	return new Promise((resolve,reject)=>{
+ 		if(confirmPass !== pass){
+			resolve("Password and Confirm password not match") 
+		}else if(pass.length<8){
+			resolve("Password must longer 8 characters")
+		}else{
+			let fd = new FormData();
+			fd.append('avatar', file)
+			fd.append('mail', mail)
+			fd.append('pass', pass)
+			fd.append('username', username)
+			fd.append('hoTen', hoTen)
+			fd.append('birth', birth)
+			fd.append('sdt', sdt)
+			$.ajax({
+				type:"POST",
+				url:"sign_up.php",
+				cache: false,
+                contentType: false,
+                processData: false,
+				data:fd,
+				success: function (response) {
+					console.log(response)
+					if(response==="File so big"){
+						resolve("File so big")
+					}else if(response.includes("Duplicate entry")){
+						resolve("Duplicate entry")
+					}else if (response === "Insert success"){
+						resolve("Insert success")
+					}else{
+						resolve(response)
+					}
+				},
+				fail: function(xhr, textStatus, errorThrown){
+					resolve("Request failed")
+				}
+			});
+			
+		}
+ 	})
+}
